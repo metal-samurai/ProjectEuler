ab338e4e05bc1dff024c986ac82d3ca730f54326
Samurai <Samurai@SAMURAI>
2022-08-24 17:04:23 -0400

    Add project files.

---
+++ ProjectEuler/ProjectEuler.cs
@@ -0,0 +1,121 @@
+﻿using System.Diagnostics;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("ProjectEulerTests")]
+namespace ProjectEuler
+{
+    public static class ProjectEuler
+    {
+        /// <summary>
+        /// Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int MultiplesOf3And5(int number)
+        {
+            var sum = 0;
+
+            for (var i = 3; i < number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0) sum += i;
+            }
+            
+            return sum;
+        }
+
+        /// <summary>
+        /// By considering the terms in the Fibonacci sequence whose values do not exceed number, find the sum of the even-valued terms.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int FiboEvenSum(int number)
+        {
+            List<int> sequence = new(new int[] { 1, 2 });
+            var sum = 0;
+            var i = 0;
+            while (sequence[i] <= number)
+            {
+                if (sequence[i] % 2 == 0)
+                {
+                    sum += sequence[i];
+                }
+
+                if (i == sequence.Count - 1)
+                {
+                    sequence.Add(sequence[i] + sequence[i - 1]);
+                    sequence.RemoveAt(0);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return sum;
+        }
+
+        internal static bool IsPrime(long number)
+        {
+            if (number < 2) return false;
+            if (number == 2) return true;
+
+            for (var i = 2; i < number; i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// What is the largest prime factor of the given number?
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static long LargestPrimeFactor(long number)
+        {
+            long largestFactor = 0;
+
+            for (var i = 2; i <= number; i++)
+            {
+                if (IsPrime(i))
+                {
+                    while (number % i == 0)
+                    {
+                        largestFactor = i;
+                        number /= i;
+                    }
+                }
+            }
+
+            return largestFactor;
+        }
+
+        /// <summary>
+        /// Find the largest palindrome made from the product of two n-digit numbers.
+        /// </summary>
+        /// <param name="digits"></param>
+        /// <returns></returns>
+        public static long? LargestPalindromeProduct(int digits)
+        {
+            var maxOperand = long.Parse(new string('9', digits));
+            var minOperand = long.Parse("1" + new string('0', digits - 1));
+            long? largestPalindrome = null;
+
+            for (var i = maxOperand; i >= minOperand; i--)
+            {
+                for (var j = maxOperand; j >= minOperand; j--)
+                {
+                    var product = i * j;
+                    
+                    if (product.ToString() == new string(product.ToString().Reverse().ToArray()))
+                    {
+                        if (largestPalindrome == null || product >= largestPalindrome) largestPalindrome = product;
+                        break;
+                    }
+                }
+            }
+
+            return largestPalindrome;
+        }
+    }
+}
---
+++ ProjectEulerTests/ProjectEulerTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Tests
+{
+    [TestClass()]
+    public class ProjectEulerTests
+    {
+        [TestMethod()]
+        public void MultiplesOf3And5Test()
+        {
+            Assert.AreEqual(543, ProjectEuler.MultiplesOf3And5(49));
+            Assert.AreEqual(233168, ProjectEuler.MultiplesOf3And5(1000));
+            Assert.AreEqual(16687353, ProjectEuler.MultiplesOf3And5(8456));
+            Assert.AreEqual(89301183, ProjectEuler.MultiplesOf3And5(19564));
+        }
+
+        [TestMethod()]
+        public void FiboEvenSumTest()
+        {
+            Assert.AreEqual(10, ProjectEuler.FiboEvenSum(8));
+            Assert.AreEqual(10, ProjectEuler.FiboEvenSum(10));
+            Assert.AreEqual(44, ProjectEuler.FiboEvenSum(34));
+            Assert.AreEqual(44, ProjectEuler.FiboEvenSum(60));
+            Assert.AreEqual(798, ProjectEuler.FiboEvenSum(1000));
+            Assert.AreEqual(60696, ProjectEuler.FiboEvenSum(100000));
+            Assert.AreEqual(4613732, ProjectEuler.FiboEvenSum(4000000));
+        }
+
+        [TestMethod()]
+        public void IsPrimeTestTrue()
+        {
+            Assert.IsTrue(ProjectEuler.IsPrime(2));
+            Assert.IsTrue(ProjectEuler.IsPrime(3));
+            Assert.IsTrue(ProjectEuler.IsPrime(5));
+            Assert.IsTrue(ProjectEuler.IsPrime(7));
+            Assert.IsTrue(ProjectEuler.IsPrime(11));
+            Assert.IsTrue(ProjectEuler.IsPrime(13));
+            Assert.IsTrue(ProjectEuler.IsPrime(29));
+        }
+
+        [TestMethod()]
+        public void IsPrimeTestFalse()
+        {
+            Assert.IsFalse(ProjectEuler.IsPrime(-1));
+            Assert.IsFalse(ProjectEuler.IsPrime(0));
+            Assert.IsFalse(ProjectEuler.IsPrime(1));
+            Assert.IsFalse(ProjectEuler.IsPrime(4));
+        }
+
+        [TestMethod()]
+        public void LargestPrimeFactorTest()
+        {
+            Assert.AreEqual(2, ProjectEuler.LargestPrimeFactor(2));
+            Assert.AreEqual(3, ProjectEuler.LargestPrimeFactor(3));
+            Assert.AreEqual(5, ProjectEuler.LargestPrimeFactor(5));
+            Assert.AreEqual(7, ProjectEuler.LargestPrimeFactor(7));
+            Assert.AreEqual(2, ProjectEuler.LargestPrimeFactor(8));
+            Assert.AreEqual(29, ProjectEuler.LargestPrimeFactor(13195));
+            Assert.AreEqual(6857, ProjectEuler.LargestPrimeFactor(600851475143));
+        }
+
+        [TestMethod()]
+        public void LargestPalindromeProduct()
+        {
+            Assert.AreEqual(9009, ProjectEuler.LargestPalindromeProduct(2));
+            Assert.AreEqual(906609, ProjectEuler.LargestPalindromeProduct(3));
+        }
+    }
+}
